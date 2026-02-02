8fbfa7a8b22b222c933d3fd84f991af76794648a
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-02 18:24:01 +0900

    エラーハンドリング重複の削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,6 +18,9 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :shipping_duration_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   # 価格のバリデーション
-  validates :price, numericality: { only_integer: true}
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
-end
+  validates :price, numericality: { 
+  only_integer: true, 
+  greater_than_or_equal_to: 300, 
+  less_than_or_equal_to: 9_999_999 
+}
+  end
