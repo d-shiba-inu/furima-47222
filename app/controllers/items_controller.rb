d270b75570a103f21866896670eefffdf2ea1169
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-02 18:23:38 +0900

    データ保存の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def item_params
+  params.require(:item).permit(
+    :image, 
+    :name, 
+    :introduce, 
+    :category_id,
+    :condition_id, 
+    :shipping_fee_status_id, 
+    :prefecture_id, 
+    :shipping_duration_id, 
+    :price
+  ).merge(user_id: current_user.id)
+end
+
   private
 
   def item_params
