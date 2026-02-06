1dce7f53ac0d3e84a33f360ecc7e3abf3545d33d
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-06 11:38:08 +0900

    データが空のとき

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
   before do
-    user = FactoryBot.create(:user)
-    item = FactoryBot.create(:item)
-    # 正常な値の組み合わせを定義する
-    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+    user = FactoryBot.build(:user)
+    item = FactoryBot.build(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: 1, item_id: 1)
   end
 
   describe '商品購入機能の保存' do
@@ -16,10 +15,53 @@ RSpec.describe OrderAddress, type: :model do
     end
 
     context '内容に問題がある場合' do
+      # 空の時のテストコード
       it 'postal_codeが空だと保存できない' do
         @order_address.postal_code = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it 'prefecture_idが空だと保存できない' do
+        @order_address.prefecture_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'cityが空だと保存できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+
+      it 'street_addressが空だと保存できない' do
+        @order_address.street_address = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Street address can't be blank")
+      end
+
+      it 'phone_numberが空だと保存できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it 'user_idが空だと保存できない' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空だと保存できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+
+      it 'tokenが空だと保存できない' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
       end
     end
   end
