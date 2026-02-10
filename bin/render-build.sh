0dae2e51bbeaafb883106b3fb7d06ba814f010f9
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-10 15:48:08 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-bundle install && bundle exec rails assets:precompile
+bundle install && bundle exec rails assets:precompile && bin/importmap
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
